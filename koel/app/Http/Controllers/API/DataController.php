cf52ab8016960995796645c282a65ff5ddc6a91f
amin abedi <amin.abedi.1368@gmail.com>
2018-04-24 15:59:53 +0430

    mostplay

--- koel/app/Http/Controllers/API/DataController.php
+++ koel/app/Http/Controllers/API/DataController.php
@@ -29,9 +29,9 @@ class DataController extends Controller
         return response()->json(MediaCache::get() + [
             'settings' => $request->user()->is_admin ? Setting::pluck('value', 'key')->all() : [],
             'playlists' => Playlist::byCurrentUser()->orderBy('name')->get()->toArray(),
-            'interactions' => Interaction::byCurrentUser()->get(),
+//            'interactions' => Interaction::byCurrentUser()->get(),
              //   SELECT *,SUM(play_count) as sum_play_count FROM interactions GROUP by song_id ORDER BY sum_play_count DESC
-            'mostPlay' => DB::table('interactions')->select('*',DB::raw('SUM(play_count) as sum_play_count'))->groupBy('song_id')
+            'interactions' => DB::table('interactions')->select('*',DB::raw('SUM(play_count) as sum_play_count'))->groupBy('song_id')
                 ->orderBy('sum_play_count', 'desc')
                 ->take(20)
                 ->get(),
